be7e92c9d5a325bf8d74687f1e3302641692a130
Lisbrun <jmedinagu@unal.edu.co>
2022-08-31 17:20:32 -0500

    Eliminadas un archivo innecesario

--- ejemplo1.py
+++ ejemplo1.py
@@ -5,18 +5,19 @@ MiConexion = sqlite3.connect("PrimeraBase")
 
 
 MiCursor = MiConexion.cursor()
-MiCursor.execute("INSERT INTO PRODUCTOS VALUES('Pan',3600,'media')")
-#productos=[
-    #("Manzana", 600, "media"),
-    #("Pera", 1200, "media"),
-   # ("Sandia", 900, "media"),
-#]
-#MiCursor.executemany("INSERT INTO PRODUCTOS VALUES (?,?,?)", productos)
+MiCursor.execute("INSERT INTO PRODUCTOS VALUES('Arroz',2000,'media')")
+productos=[
+    ("Manzana", 600, "media"),
+    ("Pera", 1200, "media"),
+    ("Sandia", 900, "media"),
+]
+print ("esto es una prueba")
+MiCursor.executemany("INSERT INTO PRODUCTOS VALUES (?,?,?)", productos)
 
 MiConexion.commit()
 
 
 
 
-
+print("se ejecuto")
 MiConexion.close()
