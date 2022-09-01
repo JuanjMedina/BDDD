e5fe85f3de9d4410ad36ac400bc73cd7bf62e18c
Lisbrun <jmedinagu@unal.edu.co>
2022-08-31 20:45:28 -0500

    comandos listos para comenzar con el proyeecto de la universidad

---
+++ llavespri.py
@@ -0,0 +1,18 @@
+import sqlite3
+from tkinter import INSERT
+miConexion = sqlite3.connect("Proyecto")
+micursor= miConexion.cursor()
+#UNIQUE
+#micursor.execute('''CREATE TABLE PRODUCTOS(ID INTEGER PRIMARY KEY AUTOINCREMENT,NOMBRE VARCHAR(20), PRECIO INTEGER(15))''')
+micursor.execute("UPDATE PRODUCTOS SET PRECIO=45 WHERE NOMBRE ='Lentejas'")
+#productos=[
+ #   ("Lentejas",1000),
+  #  ("Frijoles",5000),
+   # ("Manzanas",2500)
+#]
+micursor.execute("SELECT * FROM PRODUCTOS ")
+elementos = micursor.fetchall()
+print(elementos)
+#micursor.executemany("INSERT INTO PRODUCTOS VALUES (NULL,?,?)",productos)
+miConexion.commit()
+miConexion.close()
